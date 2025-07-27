a99f4169b671f50af6a0fcfdd8f07bd98d40be6f
Shambhavi Singh <singhshambhavi770@gmail.com>
2025-07-27 07:04:25 +0530

    Fixed Errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react'; // ✅ Add this
+import { Auth0Provider } from '@auth0/auth0-react';
 import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
 import './index.css';
